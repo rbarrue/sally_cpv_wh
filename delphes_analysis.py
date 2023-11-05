d7c2aced4a3b52968e93219e65f046899c403ef0
Ricardo Barrue <ricardo.barrue@tecnico.ulisboa.pt>
2023-11-05 19:09:15 +0000

    running delphes inside analysis script

--- delphes_analysis.py
+++ delphes_analysis.py
@@ -4,7 +4,7 @@ import logging
 
 from madminer.delphes import DelphesReader
 import argparse as ap
-import math
+import os
 
 import parton_level_analysis_met as parton
 
@@ -33,19 +33,27 @@ def get_ql_cos_deltaPlus(leptons=[],photons=[],jets=[],met=None,debug=False):
 
     return parton.get_ql_cos_deltaPlus([],leptons,photons,jets,met,debug)
 
-def process_events(event_path, setup_file_path, is_background_process=False,k_factor=1.0):
+def process_events(event_path, setup_file_path, is_background_process=False,k_factor=1.0,run_delphes=False,benchmark='sm'):
     
     reader=DelphesReader(setup_file_path)
     
     logging.info(f'event_path: {event_path}, is_background: {is_background_process}')
 
-    reader.add_sample(hepmc_filename=f'{event_path}/tag_1_pythia8_events.hepmc',
-                    sampled_from_benchmark='sm',
+    reader.add_sample(hepmc_filename=f'{event_path}/tag_1_pythia8_events.hepmc.gz',
+                    sampled_from_benchmark=benchmark,
                     is_background=is_background_process,
                     lhe_filename=f'{event_path}/unweighted_events.lhe.gz',
-                    delphes_filename=f'{event_path}/delphes_events.root',
+                    delphes_filename=None if run_delphes else f'{event_path}/delphes_events.root',
                     k_factor=k_factor,
                     weights='lhe')
+
+    if run_delphes:
+        if os.path.exists(event_path+'/tag_1_pythia8_events_delphes.root'):
+            logging.warning(f'Delphes file in {event_path} already exists !')
+        reader.run_delphes('/cvmfs/sw.el7/gcc63/madgraph/3.3.1/b01/Delphes/','cards/delphes_card_ATLAS.tcl',initial_command='module load gcc63/madgraph/3.3.1; source /cvmfs/sw.el7/gcc63/madgraph/3.3.1/b01/Delphes/DelphesEnv.sh',log_file=event_path+'/run_delphes.log')
+
+    if os.path.exists(event_path+'/analysed_events.h5'):
+        logging.warning(f'analysed (.h5) file in {event_path} already exists !')
 
     # this will have to be changed if I am to change 
     for i, name in enumerate(parton.observable_names):
@@ -59,7 +67,7 @@ def process_events(event_path, setup_file_path, is_background_process=False,k_fa
     reader.add_cut('j[0].b_tag',required=True)
     reader.add_cut('j[1].b_tag',required=True)
 
-    reader.analyse_delphes_samples()
+    reader.analyse_delphes_samples(delete_delphes_files=True)
 
     reader.save(f'{event_path}/analysed_events.h5')
 
@@ -72,6 +80,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--sample_dir',help='folder where the individual sample is',required=True)
 
+    parser.add_argument('--delphes',help='run Delphes before analysis code',default=False,action="store_true")
+
     parser.add_argument('--debug',help='output debug information',default=False,action="store_true")
 
     args=parser.parse_args()
@@ -80,7 +90,7 @@ if __name__ == '__main__':
         logging.getLogger("madminer").setLevel(logging.DEBUG)
 
     if 'background' in args.sample_dir:
-        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=True)
+        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=True,run_delphes=args.delphes)
     else:
-        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=False)
+        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=False,run_delphes=args.delphes)
     
