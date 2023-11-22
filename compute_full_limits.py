663c50425229b53973ca5c76a22a2a04c7419b8b
Ricardo Barrue <ricardo.barrue@tecnico.ulisboa.pt>
2023-11-22 15:33:08 +0000

    Changes to code calculating full limits

--- compute_full_limits.py
+++ compute_full_limits.py
@@ -64,7 +64,7 @@ def extract_limits_single_parameter(parameter_grid,p_values,index_central_point)
   list_points_inside_95_cl=[parameter_grid[i][0] if n_parameters==1 else parameter_grid[i] for i in range(len(parameter_grid)) if p_values[i] > 0.05 ]  
   return parameter_grid[index_central_point],[round(list_points_inside_68_cl[0],5),round(list_points_inside_68_cl[-1],5)],[round(list_points_inside_95_cl[0],5),round(list_points_inside_95_cl[-1],5)]
 
-  # alternative to setting the points where to calculate the likelihoods for a single coefficient
+# alternative to setting the points where to calculate the likelihoods for a single coefficient
 def get_thetas_eval(theta_min,theta_max,spacing):
   theta_max=1.2
   theta_min=-1.2
@@ -94,42 +94,39 @@ if __name__ == "__main__":
 
   parser = ap.ArgumentParser(description='Computes limits using asymptotic (large sample size) limit of the likelihood ratio as a chi-square distribution.',formatter_class=ap.ArgumentDefaultsHelpFormatter)
 
-  parser.add_argument('--main_dir',help='folder where to keep everything for MadMiner WH studies, on which we store Madgraph samples and all .h5 files (setup, analyzed events, ...)',required=True)
+  parser.add_argument('-dir','--main_dir',help='folder where to keep everything for MadMiner WH studies, on which we store Madgraph samples and all .h5 files (setup, analyzed events, ...)',required=True)
 
-  parser.add_argument('--observable_set',help="which observable sets to process in one run: full (including unobservable degrees of freedom), met (only observable degrees of freedom), or both sequentially",choices=['full','met'],default='met')
+  parser.add_argument('-pdir','--plot_dir',help='folder where to save plots to',required=True)
 
-  parser.add_argument('--channel',help='lepton/charge flavor channels to derive limits on',choices=['wph_mu','wph_e','wmh_mu','wmh_e','wmh','wph','wh_mu','wh_e','wh'],default='wh')
+  parser.add_argument('-c','--channel',help='lepton/charge flavor channels to derive limits on',choices=['wph_mu','wph_e','wmh_mu','wmh_e','wmh','wph','wh_mu','wh_e','wh'],default='wh')
 
-  parser.add_argument('--sample_type',help='sample types to process, without/with samples generated at the BSM benchmark and without/with backgrounds.',choices=['signalOnly_SMonly_noSysts_lhe','signalOnly_noSysts_lhe','withBackgrounds_SMonly_noSysts_lhe','withBackgrounds_noSysts_lhe'],nargs='+',default=['signalOnly_SMonly_noSysts_lhe','withBackgrounds_SMonly_noSysts_lhe'])
+  parser.add_argument('-s','--sample_type',help='sample types to process, without/with samples generated at the BSM benchmark and without/with backgrounds.',choices=['signalOnly_SMonly','signalOnly','withBackgrounds_SMonly','withBackgrounds'],default='signalOnly')
 
-  parser.add_argument('--mode',help='what to use to extract the limits, given as input to the expected_limits function',choices=['rate','histo','sally'],required=True)
+  parser.add_argument('-m','--mode',help='what to use to extract the limits, given as input to the expected_limits function',choices=['rate','histo','sally'],required=True)
 
-  parser.add_argument('--do_shape_only',help='if the limits are derived in shape-only histograms (i.e. if the Poisson likelihood term representing the total number of events is included in the analysis',action='store_true',default=False)
+  parser.add_argument('-shape','--shape_only',help='if the limits are derived in shape-only histograms (i.e. without Poisson likelihood term)',action='store_true',default=False)
 
-  parser.add_argument('--sally_observables',help='which of the SALLY training observable sets to use when extracting limits from a SALLY-like method',choices=['all_observables_remove_redundant_cos','kinematic_only'],default='kinematic_only',required='sally' in sys.argv)
+  parser.add_argument('-so','--sally_observables',help='which of the SALLY training observable sets to use when extracting limits from a SALLY-like method',choices=['all_observables_remove_redundant_cos','kinematic_only'],required='sally' in sys.argv)
 
-  parser.add_argument('--sally_model_name_stem',help='stem appended to name of SALLY model, to e.g. differentiate between different training settings (not input variables for training !)',required='sally' in sys.argv)
+  parser.add_argument('-sm','--sally_model',help='which of the SALLY models (for each of the input variable configurations) to use',required='sally' in sys.argv)
 
-  parser.add_argument('--observable_x',help='which of the observables to use in the x-axis if extracting limits from histograms',required='histo' in sys.argv)
+  parser.add_argument('-x','--observable_x',help='which of the observables to use in the x-axis if extracting limits from histograms',required='histo' in sys.argv)
 
-  parser.add_argument('--observable_y',help='which of the observables to use in the y-axis if extracting limits from histograms',default=None)
+  parser.add_argument('-y','--observable_y',help='which of the observables to use in the y-axis if extracting limits from histograms',default=None)
 
-  parser.add_argument('--binning_x',help='binning of the variable in the x-axis (can either be a standard observable or output of the SALLY network)',nargs="*",type=float,default=None)
+  parser.add_argument('-bx','--binning_x',help='binning of the variable in the x-axis (can either be a standard observable or output of the SALLY network)',nargs="*",type=float,default=None)
 
-  parser.add_argument('--binning_y',help='binning of the variable in the y-axis',nargs="*",type=float,default=None)
+  parser.add_argument('-by','--binning_y',help='binning of the variable in the y-axis',nargs="*",type=float,default=None)
 
-  parser.add_argument('--do_log',help='whether or not to do histograms of likelihood in log_scale',default=False,action='store_true')
+  parser.add_argument('-log','--do_log',help='whether or not to do histograms of likelihood in log_scale',default=False,action='store_true')
 
-  parser.add_argument('--n_fits',help='number of times to shuffle the input dataset and redo the fit',type=int,default=1)
+  parser.add_argument('-nf','--n_fits',help='number of times to shuffle the input dataset and redo the fit',type=int,default=1)
 
-  parser.add_argument('--debug',help='turns on debug functions',default=False,action='store_true')
+  parser.add_argument('-dbg','--debug',help='turns on debug functions',default=False,action='store_true')
 
   args=parser.parse_args()
-
-  # Partition to store h5 files and samples + path to setup file
-  main_proc_dir = f'{args.main_dir}/{args.observable_set}'
-  main_plot_dir = f'{args.main_dir}/plots/{args.observable_set}'
-  os.makedirs(f'{main_plot_dir}/limits/',exist_ok=True)
+  
+  os.makedirs(f'{args.plot_dir}/limits/',exist_ok=True)
   
   if args.observable_y is None:
     hist_vars=[args.observable_x]
@@ -139,86 +136,92 @@ if __name__ == "__main__":
     hist_bins=[args.binning_x,args.binning_y] if (args.binning_x is not None and args.binning_y is not None) else None
   logging.debug(hist_vars,hist_bins)
 
-  for sample_type in args.sample_type:
-    logging.info(f"sample type: {sample_type}")
-    list_central_values=[]
+  #for sample_type in args.sample_type:
+  logging.info(f"sample type: {args.sample_type}")
+  list_central_values=[]
 
-    if args.do_shape_only:
-      log_file_path=f"{main_plot_dir}/limits/full_limits_{args.channel}_{sample_type}_{(args.mode).replace('-','_')}_shape_only.csv"
+  if 'sally' in args.mode:
+    log_file_path=f"{args.plot_dir}/limits/full_sally_{args.sample_type}"  
+  else:
+    log_file_path=f"{args.plot_dir}/limits/full_histograms_{args.sample_type}"
+
+  if args.shape_only:
+    log_file_path += '_shape_only'
+
+  log_file=open(log_file_path+".csv",'a')
+  if os.path.getsize(log_file_path+".csv") == 0:
+    
+    if 'sally' in args.mode:
+      log_file.write('observables, model, binning_x, central value, 68% CL, 95% CL \n')
     else:
-      log_file_path=f"{main_plot_dir}/limits/full_limits_{args.channel}_{sample_type}_{(args.mode).replace('-','_')}.csv"
+      log_file.write('observable_x, binning_x, observable_y, binning_y, central value, 68% CL, 95% CL \n')
 
-    log_file=open(log_file_path,'a')
-    if os.path.getsize(log_file_path) == 0:
-      if 'sally' in args.mode:
-        log_file.write('sally model, binning_x, central value, 68% CL, 95% CL \n')
-      else:
-        log_file.write('observable_x, binning_x, observable_y, binning_y, central value, 68% CL, 95% CL \n')
+  for i in range(args.n_fits):
+    
+    # object/class to perform likelihood/limit extraction, shuffling to have different datasets
+    if i==0:
+      limits_file=AsymptoticLimits(f'{args.main_dir}/{args.channel}_{args.sample_type}.h5')
+    else:
+      combine_and_shuffle([f'{args.main_dir}/{args.channel}_{args.sample_type}.h5'],
+        f'{args.main_dir}/{args.channel}_{args.sample_type}_shuffled.h5',recalculate_header=False)
+      limits_file=AsymptoticLimits(f'{args.main_dir}/{args.channel}_{args.sample_type}_shuffled.h5')
 
-    for i in range(args.n_fits):
-      
-      # object/class to perform likelihood/limit extraction, shuffling to have different datasets
-      if i==0:
-        limits_file=AsymptoticLimits(f'{main_proc_dir}/{args.channel}_{sample_type}.h5')
-      else:
-        combine_and_shuffle([f'{main_proc_dir}/{args.channel}_{sample_type}.h5'],
-          f'{main_proc_dir}/{args.channel}_{sample_type}_shuffled.h5',recalculate_header=False)
-        limits_file=AsymptoticLimits(f'{main_proc_dir}/{args.channel}_{sample_type}_shuffled.h5')
+    parameter_grid,p_values,index_best_point,llr_kin,ll_rate,(histos, observed, observed_weights)=limits_file.expected_limits(mode=args.mode,theta_true=[0.0],
+    include_xsec=not args.shape_only,
+    model_file=f'{args.main_dir}/models/{args.sally_observables}/{args.sally_model}/sally_ensemble_{args.channel}_{args.sample_type}' if 'sally' in args.mode else None,
+    hist_vars=hist_vars if 'histo' in args.mode else None,
+    hist_bins=hist_bins,
+    return_asimov=True,n_histo_toys=None,
+    grid_ranges=[(-1.2,1.2)],grid_resolutions=[101])
+    #thetas_eval=thetas_eval,grid_resolutions=None) # can be set given min, max and spacing using the get_thetas_eval funcion
 
-      parameter_grid,p_values,index_best_point,llr_kin,ll_rate,(histos, observed, observed_weights)=limits_file.expected_limits(mode=args.mode,theta_true=[0.0],
-      include_xsec=not args.do_shape_only,
-      model_file=f'{main_proc_dir}/models/{args.sally_model_name_stem}/{args.sally_observables}/sally_ensemble_{args.channel}_{sample_type}' if 'sally' in args.mode else None,
-      hist_vars=hist_vars if 'histo' in args.mode else None,
-      hist_bins=hist_bins,
-      return_asimov=True,n_histo_toys=None,
-      grid_ranges=[(-1.2,1.2)],grid_resolutions=[101])
-      #thetas_eval=thetas_eval,grid_resolutions=None) # can be set given min, max and spacing using the get_thetas_eval funcion
+    if i%5==0:
 
-      if i%5==0:
+      # plotting the likelihood histograms for debugging
+      if args.debug:
+        
+        indices=get_indices_llr_histograms(parameter_grid,npoints_plotting=8)
 
-        # plotting the likelihood histograms for debugging
-        if args.debug:
-          
-          indices=get_indices_llr_histograms(parameter_grid,npoints_plotting=8)
-
-          likelihood_histograms = plot_histograms(
-              histos=[histos[i] for i in indices],
-              observed=[observed[i] for i in indices],
-              observed_weights=observed_weights,
-              histo_labels=[f"$cHWtil = {parameter_grid[i,0]:.2f}$" for i in indices],
-              xlabel=args.observable_x,
-              xrange=(hist_bins[0][0],hist_bins[0][-1]) if hist_bins is not None else None,
-              log=args.do_log,
-          )
-          if 'sally' in args.mode:
-            likelihood_histograms.savefig(f'{main_plot_dir}/limits/likelihoods_sally_{args.channel}_{sample_type}_{args.sally_observables}_{i}.pdf')
-          else:
-            if args.observable_y is None:
-              likelihood_histograms.savefig(f'{main_plot_dir}/limits/likelihoods_{args.channel}_{sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{i}.pdf')
-            else:
-              likelihood_histograms.savefig(f'{main_plot_dir}/limits/likelihoods_{args.channel}_{sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{args.observable_y}_{len(args.binning_y)-1}bins_{i}.pdf')
-
-        # rescaling the log likelihood such that the best fit point has ll=0
-        subtracted_ll,_=limits_file._subtract_mle(llr_kin+ll_rate)
-
-        llr_kin_histo= plot_likelihood(parameter_grid[:,0],-2.0*subtracted_ll,xlabel='cHWtil',ylabel='Rescaled -2*LLR',do_log=False)
+        likelihood_histograms = plot_histograms(
+            histos=[histos[i] for i in indices],
+            observed=[observed[i] for i in indices],
+            observed_weights=observed_weights,
+            histo_labels=[f"$cHWtil = {parameter_grid[i,0]:.2f}$" for i in indices],
+            xlabel=args.observable_x,
+            xrange=(hist_bins[0][0],hist_bins[0][-1]) if hist_bins is not None else None,
+            log=args.do_log,
+        )
         if 'sally' in args.mode:
-          llr_kin_histo.savefig(f'{main_plot_dir}/limits/llr_curve_sally_{args.channel}_{sample_type}_{args.sally_observables}_{i}.pdf')
+          likelihood_histograms.savefig(f'{args.plot_dir}/limits/likelihoods_sally_{args.channel}_{args.sample_type}_{args.sally_observables}_{i}.pdf')
         else:
           if args.observable_y is None:
-            llr_kin_histo.savefig(f'{main_plot_dir}/limits/llr_curve_{args.channel}_{sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{i}.pdf')
+            likelihood_histograms.savefig(f'{args.plot_dir}/limits/likelihoods_{args.channel}_{args.sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{i}.pdf')
           else:
-            llr_kin_histo.savefig(f'{main_plot_dir}/limits/llr_curve_{args.channel}_{sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{args.observable_y}_{len(args.binning_y)-1}bins_{i}.pdf')
+            likelihood_histograms.savefig(f'{args.plot_dir}/limits/likelihoods_{args.channel}_{args.sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{args.observable_y}_{len(args.binning_y)-1}bins_{i}.pdf')
 
-      central_value,cl_68,cl_95=extract_limits_single_parameter(parameter_grid,p_values,index_best_point)
-      logging.info(f'n_fit: {str(i)}, central value: {str(central_value)}; 68% CL: {str(cl_68)}; 95% CL: {str(cl_95)}')
-      list_central_values.append(central_value[0])
+      # rescaling the log likelihood such that the best fit point has ll=0
+      subtracted_ll,_=limits_file._subtract_mle(llr_kin+ll_rate)
+
+      llr_kin_histo= plot_likelihood(parameter_grid[:,0],-2.0*subtracted_ll,xlabel='cHWtil',ylabel='Rescaled -2*LLR',do_log=False)
 
       if 'sally' in args.mode:
-        log_file.write(f"{args.sally_model_name_stem}_{args.sally_observables}, {str(args.binning_y).replace(',',' ')}, {str(central_value[0])}, {str(cl_68).replace(',',' ')}, {str(cl_95).replace(',',' ')} \n") 
+        llr_kin_histo.savefig(f'{args.plot_dir}/limits/llr_curve_sally_{args.channel}_{args.sample_type}_{args.sally_observables}_{i}.pdf')
       else:
-        log_file.write(f"{args.observable_x}, {str(args.binning_x).replace(',',' ')}, {args.observable_y}, {str(args.binning_y).replace(',',' ')}, {str(central_value[0]).replace(',',' ')}, {str(cl_68).replace(',',' ')}, {str(cl_95).replace(',',' ')} \n") 
+        if args.observable_y is None:
+          llr_kin_histo.savefig(f'{args.plot_dir}/limits/llr_curve_{args.channel}_{args.sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{i}.pdf')
+        else:
+          llr_kin_histo.savefig(f'{args.plot_dir}/limits/llr_curve_{args.channel}_{args.sample_type}_{args.observable_x}_{len(args.binning_x)-1}bins_{args.observable_y}_{len(args.binning_y)-1}bins_{i}.pdf')
+    
+    
+    central_value,cl_68,cl_95=extract_limits_single_parameter(parameter_grid,p_values,index_best_point)
+    logging.debug(f'n_fit: {str(i)}, central value: {str(central_value)}; 68% CL: {str(cl_68)}; 95% CL: {str(cl_95)}')
+    list_central_values.append(central_value[0]) 
 
-    logging.debug("list of central values : ",str(list_central_values))
+    if 'sally' in args.mode:
+      log_file.write(f"{args.sally_observables}, {args.sally_model}, {str(args.binning_x).replace(',',' ')}, {str(central_value[0])}, {str(cl_68).replace(',',' ')}, {str(cl_95).replace(',',' ')} \n") 
+    else:
+      log_file.write(f"{args.observable_x}, {str(args.binning_x).replace(',',' ')}, {args.observable_y}, {str(args.binning_y).replace(',',' ')}, {str(central_value[0]).replace(',',' ')}, {str(cl_68).replace(',',' ')}, {str(cl_95).replace(',',' ')} \n") 
 
-    log_file.close()
+  logging.debug("list of central values : ",str(list_central_values))
+
+  log_file.close()
