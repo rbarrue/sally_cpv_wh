ffa7dc59ca586bef2576bf8820d849338d78317f
Ricardo Barrue <ricardo.barrue@tecnico.ulisboa.pt>
2025-05-29 16:18:52 +0100

    Changing Delphes analysis scripts + removing parton-level ones

--- analysis_sample_combiner.py
+++ analysis_sample_combiner.py
@@ -33,132 +33,103 @@ if __name__ == "__main__":
 
   parser.add_argument('-dir','--main_dir',help='folder where to keep everything for MadMiner WH studies, on which we store Madgraph samples and all .h5 files (setup, analyzed events, ...)',required=True)
 
-  parser.add_argument('-i','--combine_individual',help='combine samples for each of the charge+flavor combination separately (should be done once before all other combination possibilities)',action='store_true',default=False)
-
   parser.add_argument('-f','--combine_flavors',help='combine muon and electron events for each charges separately', action='store_true',default=False)
 
   parser.add_argument('-c', '--combine_charges',help='combine w+ and w- events for each flavor separately', action='store_true',default=False)
 
-  parser.add_argument('-a','--combine_all',help='combine all charges + flavors', action='store_true',default=False)
-
-  parser.add_argument('-s','--do_signal',action='store_true',help='run over signals',default=False)
-
-  parser.add_argument('-b','--do_backgrounds',action='store_true',help='run over backgrounds',default=False)
-
   args=parser.parse_args()
 
-  if not args.combine_individual and (args.combine_flavors or args.combine_charges or args.combine_all):
-    logging.warning('not asking to combine samples for each of the charge+flavor combination separately, but asking to make a combination of samples from different charges or flavors. watch out for possible crash from missing samples')
-  
-  if args.combine_flavors and args.combine_charges:
-    if not args.combine_all:
-      logging.warning('asked to combine samples in flavor and in charge separately but combine_all=False, won\'t create single WH sample')
-  
-  if not (args.do_signal or args.do_backgrounds):
-    logging.error("didnt ask for signal or backgrounds, choose one and rerun")
-    sys.exit(1)
-
   flavor_charge_combinations = list(product(('mu','e'),('p','m')))
-   
-   # combining individual run samples
-  if args.combine_individual:
-
-    if args.do_signal:
-      signal_samples = [f'w{charge}h_{flavor}' for (flavor,charge) in flavor_charge_combinations]
-      for sample in signal_samples:
-        event_folder = f'{args.main_dir}/signal_samples/{sample}_smeftsim_SM/Events'
-        list_samples_to_combine = [f'{event_folder}/{run}/analysed_events.h5' for run in os.listdir(event_folder)]
-        logging.warning(f'found {len(list_samples_to_combine)} runs for {sample}. weighting each by the inverse of the number of runs')
-        combine_and_shuffle(list_samples_to_combine,f'{args.main_dir}/{sample}_signalOnly.h5',k_factors=1.0/len(list_samples_to_combine))
   
-    if args.do_backgrounds:
-      background_samples=[f't{charge}b_{flavor}' for (flavor,charge) in flavor_charge_combinations]
-      background_samples+=[f'tt_{flavor}{charge}jj' for (flavor,charge) in flavor_charge_combinations]
-      background_samples+=[f'w{charge}bb_{flavor}' for (flavor,charge) in flavor_charge_combinations]
-      
-      for sample in background_samples:
-        event_folder = f'{args.main_dir}/background_samples/{sample}_background/Events'
-        list_samples_to_combine = [f'{event_folder}/{run}/analysed_events.h5' for run in os.listdir(event_folder)]
-        logging.warning(f'found {len(list_samples_to_combine)} runs for {sample}. weighting each by the inverse of the number of runs')
-        combine_and_shuffle(list_samples_to_combine,f'{args.main_dir}/background_samples/{sample}_background.h5',k_factors=1.0/len(list_samples_to_combine))
+  # signal samples
+  signal_samples = [f'w{charge}h_{flavor}' for (flavor,charge) in flavor_charge_combinations]
+  for sample in signal_samples:
+    event_folder = f'{args.main_dir}/signal_samples/{sample}_smeftsim_SM/Events'
+    list_samples_to_combine = [f'{event_folder}/{run}/analysed_events.h5' for run in os.listdir(event_folder)]
+    logging.warning(f'found {len(list_samples_to_combine)} runs for {sample}. weighting each by the inverse of the number of runs')
+    combine_and_shuffle(list_samples_to_combine,f'{args.main_dir}/{sample}_signalOnly.h5',k_factors=1.0/len(list_samples_to_combine))
 
-      for (flavor,charge) in flavor_charge_combinations:
-        combine_and_shuffle([
-          f'{args.main_dir}/background_samples/t{charge}b_{flavor}_background.h5',
-          f'{args.main_dir}/background_samples/tt_{flavor}{charge}jj_background.h5',
-          f'{args.main_dir}/background_samples/w{charge}bb_{flavor}_background.h5'],
-          f'{args.main_dir}/w{charge}h_{flavor}_backgroundOnly.h5'
-        )
+  # background samples
+  background_samples=[f't{charge}b_{flavor}' for (flavor,charge) in flavor_charge_combinations]
+  background_samples+=[f'tt_{flavor}{charge}jj' for (flavor,charge) in flavor_charge_combinations]
+  background_samples+=[f'w{charge}bb_{flavor}' for (flavor,charge) in flavor_charge_combinations]
+  
+  for sample in background_samples:
+    event_folder = f'{args.main_dir}/background_samples/{sample}_background/Events'
+    list_samples_to_combine = [f'{event_folder}/{run}/analysed_events.h5' for run in os.listdir(event_folder)]
+    logging.warning(f'found {len(list_samples_to_combine)} runs for {sample}. weighting each by the inverse of the number of runs')
+    combine_and_shuffle(list_samples_to_combine,f'{args.main_dir}/background_samples/{sample}_background.h5',k_factors=1.0/len(list_samples_to_combine))
 
-      if args.do_signal:
-        combine_and_shuffle([
-          f'{args.main_dir}/w{charge}h_{flavor}_signalOnly.h5',
-          f'{args.main_dir}/w{charge}h_{flavor}_backgroundOnly.h5'],
-          f'{args.main_dir}/w{charge}h_{flavor}_withBackgrounds.h5'
-        )
+  for (flavor,charge) in flavor_charge_combinations:
+    combine_and_shuffle([
+      f'{args.main_dir}/background_samples/t{charge}b_{flavor}_background.h5',
+      f'{args.main_dir}/background_samples/tt_{flavor}{charge}jj_background.h5',
+      f'{args.main_dir}/background_samples/w{charge}bb_{flavor}_background.h5'],
+      f'{args.main_dir}/w{charge}h_{flavor}_backgroundOnly.h5'
+    )
 
-    logging.info('finished standard sample combination for training of ML methods, will now start the combinations used for plotting')
+  # combining signals and backgrounds for each charge/flavor pair
+  combine_and_shuffle([
+    f'{args.main_dir}/w{charge}h_{flavor}_signalOnly.h5',
+    f'{args.main_dir}/w{charge}h_{flavor}_backgroundOnly.h5'],
+    f'{args.main_dir}/w{charge}h_{flavor}_withBackgrounds.h5'
+  )
+
+  # combining charge/flavor pairs - used to train the SALLY models 
+  combine_and_shuffle([f'{args.main_dir}/w{charge}h_{flavor}_signalOnly.h5'
+                      for (flavor,charge) in flavor_charge_combinations],
+                      f'{args.main_dir}/wh_signalOnly.h5')
+
+  combine_and_shuffle([f'{args.main_dir}/w{charge}h_{flavor}_backgroundOnly.h5'
+                    for (flavor,charge) in flavor_charge_combinations],
+                  f'{args.main_dir}/wh_backgroundOnly.h5')
+  
+
+  combine_and_shuffle([f'{args.main_dir}/wh_signalOnly.h5',f'{args.main_dir}/wh_backgroundOnly.h5'],
+                f'{args.main_dir}/wh_withBackgrounds.h5')
+
+  logging.info('finished standard sample combination for training of ML methods, will now start the combinations used for plotting')
 
   if args.combine_flavors:
+    logging.info("Combining samples with same charge and different flavors")
     for charge in ('p','m'):
 
-      if args.do_signal:
-        combine_and_shuffle([
-          f'{args.main_dir}/w{charge}h_e_signalOnly.h5',
-          f'{args.main_dir}/w{charge}h_mu_signalOnly.h5'],
-          f'{args.main_dir}/w{charge}h_signalOnly.h5'
-        )
+      combine_and_shuffle([
+        f'{args.main_dir}/w{charge}h_e_signalOnly.h5',
+        f'{args.main_dir}/w{charge}h_mu_signalOnly.h5'],
+        f'{args.main_dir}/w{charge}h_signalOnly.h5'
+      )
 
-      if args.do_backgrounds:
-        combine_and_shuffle([
-          f'{args.main_dir}/w{charge}h_e_backgroundOnly.h5',
-          f'{args.main_dir}/w{charge}h_mu_backgroundOnly.h5'],
-          f'{args.main_dir}/w{charge}h_backgroundOnly.h5'
-        )
+      combine_and_shuffle([
+        f'{args.main_dir}/w{charge}h_e_backgroundOnly.h5',
+        f'{args.main_dir}/w{charge}h_mu_backgroundOnly.h5'],
+        f'{args.main_dir}/w{charge}h_backgroundOnly.h5'
+      )
 
-        if args.do_signal:
-          combine_and_shuffle([
-            f'{args.main_dir}/w{charge}h_signalOnly.h5',
-            f'{args.main_dir}/w{charge}h_backgroundOnly.h5'],
-            f'{args.main_dir}/w{charge}h_withBackgrounds.h5'
-          )
-
+      combine_and_shuffle([
+        f'{args.main_dir}/w{charge}h_signalOnly.h5',
+        f'{args.main_dir}/w{charge}h_backgroundOnly.h5'],
+        f'{args.main_dir}/w{charge}h_withBackgrounds.h5'
+      )
+  
   if args.combine_charges:
+    logging.info("Combining samples with opposite charge and same flavor")
     for flavor in ('e','mu'):
 
-      if args.do_signal:
-        combine_and_shuffle([
-          f'{args.main_dir}/wph_{flavor}_signalOnly.h5',
-          f'{args.main_dir}/wmh_{flavor}_signalOnly.h5'],
-          f'{args.main_dir}/wh_{flavor}_signalOnly.h5'
-        )
+      combine_and_shuffle([
+        f'{args.main_dir}/wph_{flavor}_signalOnly.h5',
+        f'{args.main_dir}/wmh_{flavor}_signalOnly.h5'],
+        f'{args.main_dir}/wh_{flavor}_signalOnly.h5'
+      )
 
-      if args.do_backgrounds:
-        combine_and_shuffle([
-          f'{args.main_dir}/wph_{flavor}_backgroundOnly.h5',
-          f'{args.main_dir}/wmh_{flavor}_backgroundOnly.h5'],
-          f'{args.main_dir}/wh_{flavor}_backgroundOnly.h5'
-        )
+      combine_and_shuffle([
+        f'{args.main_dir}/wph_{flavor}_backgroundOnly.h5',
+        f'{args.main_dir}/wmh_{flavor}_backgroundOnly.h5'],
+        f'{args.main_dir}/wh_{flavor}_backgroundOnly.h5'
+      )
 
-        if args.do_signal:
-          combine_and_shuffle([
-            f'{args.main_dir}/wh_{flavor}_signalOnly.h5',
-            f'{args.main_dir}/wh_{flavor}_backgroundOnly.h5'],
-            f'{args.main_dir}/wh_{flavor}_withBackgrounds.h5'
-          )
-  
-  if args.combine_all:
-
-    if args.do_signal:
-      combine_and_shuffle([f'{args.main_dir}/w{charge}h_{flavor}_signalOnly.h5'
-                          for (flavor,charge) in flavor_charge_combinations],
-                          f'{args.main_dir}/wh_signalOnly.h5')
-    
-    if args.do_backgrounds:
-      combine_and_shuffle([f'{args.main_dir}/w{charge}h_{flavor}_backgroundOnly.h5'
-                        for (flavor,charge) in flavor_charge_combinations],
-                      f'{args.main_dir}/wh_backgroundOnly.h5')
-      
-      if args.do_signal:
-        combine_and_shuffle([f'{args.main_dir}/wh_signalOnly.h5',f'{args.main_dir}/wh_backgroundOnly.h5'],
-                    f'{args.main_dir}/wh_withBackgrounds.h5')
+      combine_and_shuffle([
+        f'{args.main_dir}/wh_{flavor}_signalOnly.h5',
+        f'{args.main_dir}/wh_{flavor}_backgroundOnly.h5'],
+        f'{args.main_dir}/wh_{flavor}_withBackgrounds.h5'
+      )
--- delphes_analysis.py
+++ delphes_analysis.py
@@ -6,7 +6,11 @@ from madminer.delphes import DelphesReader
 import argparse as ap
 import os
 
-import parton_level_analysis_met as parton
+import numpy as np
+from math import sqrt, fabs
+
+from madminer.utils.particle import MadMinerParticle
+import vector
 
 # MadMiner output
 logging.basicConfig(
@@ -20,28 +24,231 @@ for key in logging.Logger.manager.loggerDict:
     if "madminer" not in key:
         logging.getLogger(key).setLevel(logging.WARNING)
 
-# redefining here since for Delphes files, the particles input variable (existent in LHE files) is not used anymore
+# Elements in the lists will be given as input to the add_observable function
+observable_names = [
+    'b1_px', 'b1_py', 'b1_pz', 'b1_e',
+    'b2_px', 'b2_py', 'b2_pz', 'b2_e',
+    'l_px', 'l_py', 'l_pz', 'l_e',
+    'v_px', 'v_py',
+    'pt_b1', 'pt_b2', 'pt_l', 'met', 'pt_w', 'pt_h',
+    'eta_b1', 'eta_b2', 'eta_l', 'eta_h',
+    'phi_b1', 'phi_b2', 'phi_l', 'phi_v', 'phi_w', 'phi_h',
+    'theta_b1', 'theta_b2', 'theta_l', 'theta_h',
+    'dphi_bb', 'dphi_lv', 'dphi_wh',
+    'm_bb', 'mt_lv', 'mt_tot',
+    'q_l',
+    'dphi_lb1', 'dphi_lb2', 'dphi_vb1', 'dphi_vb2',
+    'dR_bb', 'dR_lb1', 'dR_lb2'
+]
+
+list_of_observables = [
+    'j[0].px', 'j[0].py', 'j[0].pz', 'j[0].e',
+    'j[1].px', 'j[1].py', 'j[1].pz', 'j[1].e',
+    'l[0].px', 'l[0].py', 'l[0].pz', 'l[0].e',
+    'met.px', 'met.py',   
+    'j[0].pt', 'j[1].pt', 'l[0].pt', 'met.pt', '(l[0] + met).pt', '(j[0] + j[1]).pt',
+    'j[0].eta', 'j[1].eta', 'l[0].eta', '(j[0] + j[1]).eta',
+    'j[0].phi', 'j[1].phi', 'l[0].phi', 'met.phi', '(l[0] + met).phi', '(j[0] + j[1]).phi',
+    'j[0].theta', 'j[1].theta', 'l[0].theta', '(j[0] + j[1]).theta',
+    'j[0].deltaphi(j[1])', 'l[0].deltaphi(met)', '(l[0] + met).deltaphi(j[0] + j[1])',
+    '(j[0] + j[1]).m', '(l[0] + met).mt', '(j[0] + j[1] + l[0] + met).mt',
+    'l[0].charge',
+    'l[0].deltaphi(j[0])', 'l[0].deltaphi(j[1])', 'met.deltaphi(j[0])', 'met.deltaphi(j[1])',
+    'j[0].deltaR(j[1])', 'l[0].deltaR(j[0])', 'l[0].deltaR(j[1])'
+]
+
+# solves quadratic equation to get two possible solutions for pz_nu
+# calculation similar to what is done in the ATLAS VHbb analyses (getNeutrinoPz function in AnalysisReader.cxx)
 def get_neutrino_pz(leptons=[],photons=[],jets=[],met=None,debug=False):
-    
-    return parton.get_neutrino_pz([],leptons,photons,jets,met,debug)
+
+  if (met is None) or leptons==[] or jets==[]:
+      raise TypeError("one of the required inputs of wrong type (not found)")
+  
+  # W boson mass (GeV)
+  m_w=80.379
+  
+  pz_nu_1=0.0
+  pz_nu_2=0.0
+
+  if debug:
+    logging.debug(f'leading lepton 4-vector: {leptons[0]}')
+    logging.debug(f'leading jet 4-vector: {jets[0]}')
+    logging.debug(f'subleading jet 4-vector: {jets[1]}')
+    logging.debug(f'met 4-vector: {met}')
+
+  # auxiliary quantities
+  mu = 0.5 * pow(m_w,2) + (leptons[0].px*met.px + leptons[0].py*met.py) # = tmp/2
+  delta = pow(mu*leptons[0].pz / pow(leptons[0].pt,2) ,2) - (pow(leptons[0].e,2)*pow(met.pt,2) - pow(mu,2))/pow(leptons[0].pt,2) 
+
+  # neglecting any imaginary parts (equivalent to setting mtw = mw)
+  if delta < 0.0:
+    delta=0.0
+  
+  pz_nu_1=mu*leptons[0].pz/pow(leptons[0].pt,2) + sqrt(delta)
+  pz_nu_2=mu*leptons[0].pz/pow(leptons[0].pt,2) - sqrt(delta)
+
+  # NOTE: MadMinerParticle inherits from Vector class in scikit-hep)
+  nu_1_vec = vector.obj(px=met.px,py=met.py,pz=pz_nu_1,m=0.0)
+  nu_2_vec = vector.obj(px=met.px,py=met.py,pz=pz_nu_2,m=0.0)
+
+  nu_1=MadMinerParticle(nu_1_vec.azimuthal,nu_1_vec.longitudinal,nu_1_vec.temporal)
+  nu_2=MadMinerParticle(nu_2_vec.azimuthal,nu_2_vec.longitudinal,nu_2_vec.temporal)
+
+  if debug:
+    logging.debug(f'nu_1 4-vector: {nu_1}; nu_2 4-vector: {nu_2}')
+
+  h_candidate=jets[0]+jets[1]
+
+  w_candidate_1=leptons[0]+nu_1
+  w_candidate_2=leptons[0]+nu_2
+
+  # correct definition of betaZ, but neutrino pZ reconstruction studies in NLO SM sample showed that this is not optimal
+  """
+  h_candidate_betaZ=h_candidate.to_beta3().z
+
+  w_candidate_1_betaZ=w_candidate_1.to_beta3().z
+  w_candidate_2_betaZ=w_candidate_2.to_beta3().z
+  """
+  h_candidate_betaZ=h_candidate.pz/np.sqrt(pow(h_candidate.pz,2)+h_candidate.M2) 
+  w_candidate_1_betaZ=w_candidate_1.pz/np.sqrt(pow(w_candidate_1.pz,2)+w_candidate_1.M2) 
+  w_candidate_2_betaZ=w_candidate_2.pz/np.sqrt(pow(w_candidate_2.pz,2)+w_candidate_2.M2) 
+
+  
+  if debug:
+    logging.debug(f'h_candidate 4-vector: {h_candidate}')
+    logging.debug(f'w_candidate_1 4-vector: {w_candidate_1}; w_candidate_2 4-vector: {w_candidate_2}')
+
+    logging.debug(f'h_candidate_BetaZ: {h_candidate_betaZ}')
+
+    logging.debug(f'w_candidate_1_BetaZ: {w_candidate_1_betaZ}')
+    logging.debug(f'w_candidate_2_BetaZ: {w_candidate_2_betaZ}')
+  
+
+  dBetaZ_1=fabs(w_candidate_1_betaZ - h_candidate_betaZ)
+  dBetaZ_2=fabs(w_candidate_2_betaZ - h_candidate_betaZ)
+
+  if debug:
+    logging.debug(f'dBetaZ_1: {dBetaZ_1}; dBetaZ_2: {dBetaZ_2}')
+
+  if dBetaZ_1 <= dBetaZ_2:
+    if debug:
+      logging.debug(f'pz_nu: {pz_nu_1}')
+    return pz_nu_1
+  else:
+    if debug:
+      print(f'pz_nu: {pz_nu_2}')
+    return pz_nu_2
+
+def get_cos_thetaStar(leptons=[],photons=[],jets=[],met=None,debug=False):
+
+  pz_nu=get_neutrino_pz(leptons=leptons,jets=jets,met=met,debug=debug)
+
+  nu_vec = vector.obj(px=met.px,py=met.py,pz=pz_nu,m=0.0)
+  nu=MadMinerParticle(nu_vec.azimuthal,nu_vec.longitudinal,nu_vec.temporal)
+
+  w_candidate=leptons[0]+nu
+  # equivalent to boostCM_of_p4(w_candidate), which hasn't been implemented yet for MomentumObject4D
+  # negating spatial part only to boost *into* the CM, default would be to boost *away* from the CM
+  lead_lepton_w_centerOfMass=leptons[0].boost_beta3(-w_candidate.to_beta3())
+
+  if debug:
+
+    logging.debug(f'W candidate 4-vector: {w_candidate}')
+    w_candidate_w_centerOfMass=w_candidate.boost_beta3(-w_candidate.to_beta3())
+    print(f'W candidate 4-vector boosted to the CM of the W candidate (expect[0.0,0.0,0.0,m_w]): {w_candidate_w_centerOfMass}')
+    print(f'leading lepton 4-vector boosted to the CM of the W candidate: {lead_lepton_w_centerOfMass}')  
+  
+  cos_thetaStar=lead_lepton_w_centerOfMass.to_Vector3D().dot(w_candidate.to_Vector3D())/(fabs(lead_lepton_w_centerOfMass.p) * fabs(w_candidate.p))
+
+  if debug:
+    print(f'cos_thetaStar= {cos_thetaStar}')
+
+  return cos_thetaStar
+
+def get_ql_cos_thetaStar(leptons=[],photons=[],jets=[],met=None,debug=False):
+
+  cos_thetaStar=get_cos_thetaStar(leptons=leptons,jets=jets,met=met)
+
+  if debug:
+    logging.debug(f'ql_cos_thetaStar = {leptons[0].charge * cos_thetaStar}')
+  return leptons[0].charge * cos_thetaStar
 
 def get_cos_deltaPlus(leptons=[],photons=[],jets=[],met=None,debug=False):
+  
+  pz_nu=get_neutrino_pz(leptons=leptons,jets=jets,met=met,debug=debug)
 
-    return parton.get_cos_deltaPlus([],leptons,photons,jets,met,debug)
+  nu_vec = vector.obj(px=met.px,py=met.py,pz=pz_nu,m=0.0)
+  nu=MadMinerParticle(nu_vec.azimuthal,nu_vec.longitudinal,nu_vec.temporal)
+
+  w_candidate=leptons[0]+nu
+  # equivalent to boostCM_of_p4(w_candidate), which hasn't been implemented yet for MomentumObject4D
+  # negating spatial part only to boost *into* the CM, default would be to boost *away* from the CM
+  lead_lepton_w_centerOfMass=leptons[0].boost_beta3(-w_candidate.to_beta3())
+
+  if debug:
+    logging.debug(f'W candidate 4-vector: {w_candidate}')
+    w_candidate_w_centerOfMass=w_candidate.boost_beta3(-w_candidate.to_beta3())
+    logging.debug(f'W candidate 4-vector boosted to the CM of the W candidate (expect[0.0,0.0,0.0,m_w]): {w_candidate_w_centerOfMass}')
+    logging.debug(f'leading lepton 4-vector boosted to the CM of the W candidate: {lead_lepton_w_centerOfMass}')
+  
+  h_candidate=jets[0]+jets[1]
+
+  h_cross_w=h_candidate.to_Vector3D().cross(w_candidate.to_Vector3D())
+
+  cos_deltaPlus=lead_lepton_w_centerOfMass.to_Vector3D().dot(h_cross_w)/(fabs(lead_lepton_w_centerOfMass.p) * fabs(h_cross_w.mag))
+
+  if debug:
+    logging.debug(f'cos_deltaPlus = {cos_deltaPlus}')
+
+  return cos_deltaPlus
 
 def get_ql_cos_deltaPlus(leptons=[],photons=[],jets=[],met=None,debug=False):
 
-    return parton.get_ql_cos_deltaPlus([],leptons,photons,jets,met,debug)
+  cos_deltaPlus=get_cos_deltaPlus(leptons=leptons,jets=jets,met=met)
+  if debug:
+    logging.debug(f'ql_cos_deltaPlus = {leptons[0].charge * cos_deltaPlus}')
+  return leptons[0].charge * cos_deltaPlus
 
 def get_cos_deltaMinus(leptons=[],photons=[],jets=[],met=None,debug=False):
 
-    return parton.get_cos_deltaMinus([],leptons,photons,jets,met,debug)
+  pz_nu=get_neutrino_pz(leptons=leptons,jets=jets,met=met,debug=debug)
+
+  nu_vec = vector.obj(px=met.px,py=met.py,pz=pz_nu,m=0.0)
+  nu=MadMinerParticle(nu_vec.azimuthal,nu_vec.longitudinal,nu_vec.temporal)
+
+  w_candidate=leptons[0]+nu
+
+  h_candidate=jets[0]+jets[1]
+
+  if debug:
+
+    logging.debug(f'H candidate 4-vector: {h_candidate}')
+    h_candidate_h_centerOfMass=h_candidate.boost_beta3(-h_candidate.to_beta3())
+    logging.debug(f'H candidate 4-vector boosted to the CM of the H candidate (expect[0.0,0.0,0.0,m_H]): {h_candidate_h_centerOfMass}')
+    
+  lead_lepton_inv_h_centerOfMass=leptons[0].boost_beta3(h_candidate.to_beta3())
+  nu_inv_h_centerOfMass=nu.boost_beta3(h_candidate.to_beta3())
+
+  if debug:  
+    logging.debug(f'leading lepton 4-vector boosted to the CM of the H candidate with an inverted momentum: {lead_lepton_inv_h_centerOfMass}')
+    logging.debug(f'neutrino 4-vector boosted to the CM of the H candidate with an inverted momentum: {nu_inv_h_centerOfMass}')
+
+  lep_cross_nu_inv_h_centerOfMass=lead_lepton_inv_h_centerOfMass.to_Vector3D().cross(nu_inv_h_centerOfMass.to_Vector3D())
+
+  cos_deltaMinus=lep_cross_nu_inv_h_centerOfMass.dot(w_candidate.to_Vector3D())/(fabs(lep_cross_nu_inv_h_centerOfMass.mag)*fabs(w_candidate.p))
+
+  if debug:
+    logging.debug(f'cos_deltaMinus = {cos_deltaMinus}')
+
+  return cos_deltaMinus
 
 def get_ql_cos_deltaMinus(leptons=[],photons=[],jets=[],met=None,debug=False):
+  cos_deltaMinus=get_cos_deltaMinus(leptons=leptons,jets=jets,met=met)
+  if debug:
+    logging.debug(f'ql_cos_deltaMinus = {leptons[0].charge * cos_deltaMinus}')
+  return leptons[0].charge * cos_deltaMinus
 
-    return parton.get_ql_cos_deltaMinus([],leptons,photons,jets,met,debug)
-
-def process_events(event_path, setup_file_path, is_background_process=False, k_factor=1.0, do_delphes=False, delphes_card='cards/delphes_card_ATLAS.tcl', benchmark='sm'):
+def process_events(event_path, setup_file_path, delphes_dir, is_background_process=False, skip_delphes=False, delphes_card='cards/delphes_card_ATLAS.tcl', benchmark='sm',initial_command=None):
     
     reader=DelphesReader(setup_file_path)
     
@@ -51,32 +258,30 @@ def process_events(event_path, setup_file_path, is_background_process=False, k_f
                     sampled_from_benchmark=benchmark,
                     is_background=is_background_process,
                     lhe_filename=f'{event_path}/unweighted_events.lhe.gz',
-                    delphes_filename=None if do_delphes else f'{event_path}/delphes_events.root',
-                    k_factor=k_factor,
+                    delphes_filename=f'{event_path}/tag_1_pythia8_events_delphes.root' if skip_delphes else None,
                     weights='lhe')
 
-    if do_delphes:
-        if os.path.exists(event_path+'/tag_1_pythia8_events_delphes.root'):
-            logging.warning(f'Delphes file in {event_path} already exists !')
-        reader.run_delphes('/cvmfs/sw.el7/gcc63/madgraph/3.3.1/b01/Delphes/', delphes_card, initial_command='module load gcc63/madgraph/3.3.1; source /cvmfs/sw.el7/gcc63/madgraph/3.3.1/b01/Delphes/DelphesEnv.sh', log_file=event_path+'/do_delphes.log')
+    if os.path.exists(f'{event_path}/tag_1_pythia8_events_delphes.root'):
+        logging.warning(f'Delphes file in {event_path} already exists !')
+    if not skip_delphes:
+        reader.run_delphes(delphes_dir, delphes_card, initial_command=initial_command, log_file=event_path+'/do_delphes.log')
 
     if os.path.exists(event_path+'/analysed_events.h5'):
         logging.warning(f'analysed (.h5) file in {event_path} already exists !')
 
     # this will have to be changed if I am to change 
-    for i, name in enumerate(parton.observable_names):
-        reader.add_observable( name, parton.list_of_observables[i], required=True )
+    for i, name in enumerate(observable_names):
+        reader.add_observable( name, list_of_observables[i], required=True )
 
     reader.add_observable_from_function('pz_nu', get_neutrino_pz,required=True)
-    reader.add_observable_from_function('cos_deltaPlus', get_cos_deltaPlus,required=True)
     reader.add_observable_from_function('ql_cos_deltaPlus', get_ql_cos_deltaPlus,required=True)
-    reader.add_observable_from_function('cos_deltaMinus', get_cos_deltaMinus,required=True)
     reader.add_observable_from_function('ql_cos_deltaMinus', get_ql_cos_deltaMinus,required=True)
 
     # requiring the two leading jets to be b-tagged
     reader.add_cut('j[0].b_tag',required=True)
     reader.add_cut('j[1].b_tag',required=True)
 
+    # deleting Delphes files to save space
     reader.analyse_delphes_samples(delete_delphes_files=True)
 
     reader.save(f'{event_path}/analysed_events.h5')
@@ -88,8 +293,10 @@ if __name__ == '__main__':
     parser.add_argument('--main_dir',help='folder where to keep everything for MadMiner WH studies, on which we store Madgraph samples and all .h5 files (setup, analyzed events, ...)', required=True)
 
     parser.add_argument('--sample_dir',help='folder where the individual sample is', required=True)
+    
+    parser.add_argument('--delphes_dir',help='folder where Delphes is installed', required=True)
 
-    parser.add_argument('--do_delphes',help='run Delphes before analysis code', default=False, action="store_true")
+    parser.add_argument('--skip_delphes',help='skip Delphes before running analysis code if Delphes was run standalone', default=False, action="store_true")
 
     parser.add_argument('--delphes_card',help='path of Delphes card', default='cards/delphes_card_ATLAS.tcl')
 
@@ -101,7 +308,7 @@ if __name__ == '__main__':
         logging.getLogger("madminer").setLevel(logging.DEBUG)
 
     if 'background' in args.sample_dir:
-        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=True,do_delphes=args.do_delphes, delphes_card=args.delphes_card)
+        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=True,skip_delphes=args.skip_delphes, delphes_card=args.delphes_card)
     else:
-        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=False,do_delphes=args.do_delphes, delphes_card=args.delphes_card)
+        process_events(f'{args.sample_dir}',f'{args.main_dir}/setup.h5',is_background_process=False,skip_delphes=args.skip_delphes, delphes_card=args.delphes_card)
     
